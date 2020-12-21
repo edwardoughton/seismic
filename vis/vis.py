71d0608a34854fc572d02e0d53004b286fa25f46
ej550 <edward.oughton@gmail.com>
2020-12-21 21:24:17 +0000

    Correct color buckets

--- vis/vis.py
+++ vis/vis.py
@@ -50,20 +50,20 @@ def generate_settlement_plot(countries, categories, filename_out):
         data = pd.read_csv(data_path)
         data = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.lon, data.lat), crs='epsg:4326')
 
-        data.query(categories[0][0]).plot(color='black', markersize=10, ax=axs[x])
-        data.query(categories[1][0]).plot(color='maroon', markersize=8, ax=axs[x])
-        data.query(categories[2][0]).plot(color='red', markersize=6, ax=axs[x])
-        data.query(categories[3][0]).plot(color='orangered', markersize=4, ax=axs[x])
-        data.query(categories[4][0]).plot(color='lightpink', markersize=2, ax=axs[x])
-        data.query(categories[5][0]).plot(color='white', markersize=1, ax=axs[x])
+        data.query(categories[5][0]).plot(color='gainsboro', markersize=0.5, ax=axs[x])
+        data.query(categories[4][0]).plot(color='lightpink', markersize=1, ax=axs[x])
+        data.query(categories[3][0]).plot(color='orangered', markersize=2, ax=axs[x])
+        data.query(categories[2][0]).plot(color='red', markersize=4, ax=axs[x])
+        data.query(categories[1][0]).plot(color='maroon', markersize=6, ax=axs[x])
+        data.query(categories[0][0]).plot(color='black', markersize=8, ax=axs[x])
 
         axs[x].legend([
-            categories[0][1],
-            categories[1][1],
-            categories[2][1],
-            categories[3][1],
-            categories[4][1],
             categories[5][1],
+            categories[4][1],
+            categories[3][1],
+            categories[2][1],
+            categories[1][1],
+            categories[0][1],
         ], prop={'size': 10})
 
         axs[x].set_title(plot_title)
@@ -106,12 +106,12 @@ if __name__ == '__main__':
     ]
 
     categories = [
-        ('0 <= phones < 500', '<0.5k'),
-        ('500 <= phones < 1000', '<1k'),
-        ('1000 <= phones < 5000', '<5k'),
-        ('5000 <= phones < 10000', '<10k'),
-        ('10000 <= phones < 20000', '<20k'),
         ('20000 <= phones', '>20k'),
+        ('10000 <= phones < 20000', '<20k'),
+        ('5000 <= phones < 10000', '<10k'),
+        ('1000 <= phones < 5000', '<5k'),
+        ('500 <= phones < 1000', '<1k'),
+        ('0 <= phones < 500', '<0.5k'),
     ]
 
     filename_out = 'phone_adoption.png'
@@ -124,12 +124,12 @@ if __name__ == '__main__':
     ]
 
     categories = [
-        ('0 <= smartphones < 500', '<0.5k'),
-        ('500 <= smartphones < 1000', '<1k'),
-        ('1000 <= smartphones < 5000', '<5k'),
-        ('5000 <= smartphones < 10000', '<10k'),
-        ('10000 <= smartphones < 20000', '<20k'),
         ('20000 <= smartphones', '>20k'),
+        ('10000 <= smartphones < 20000', '<20k'),
+        ('5000 <= smartphones < 10000', '<10k'),
+        ('1000 <= smartphones < 5000', '<5k'),
+        ('500 <= smartphones < 1000', '<1k'),
+        ('0 <= smartphones < 500', '<0.5k'),
     ]
 
     filename_out = 'smartphone_adoption.png'
